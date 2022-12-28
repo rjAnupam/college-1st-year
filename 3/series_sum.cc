fe76c99ee279589c9140793e6b985e853e8e2687
Anupam1704 <94778317+Anupam1704@users.noreply.github.com>
2022-12-28 10:24:20 +0530

    Create series_sum.cc

---
+++ 3/series_sum.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(void)
+{
+	int n;
+	cout <<"Enter number of terms for (1+2+3+4+.....): ";
+	cin >>n;
+
+	int sum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		sum += i;
+	}
+
+	cout <<"Sum: " <<sum <<endl;
+
+	return 0;
+}
