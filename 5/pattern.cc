aabe138cfd6e69a4b3e7e4950b119ff65e054c3f
rjAnupam <94778317+rjAnupam@users.noreply.github.com>
2023-01-03 21:16:46 +0530

    Create pattern.cc

---
+++ 5/pattern.cc
@@ -0,0 +1,30 @@
+/* Program to print pattern
+     1
+     2 3
+     4 5 6
+     7 8 9 10
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(void)
+{
+    int n;
+    cout <<"Height: ";
+    cin >>n;
+
+    int c = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout <<c <<" ";
+            c++;
+        }
+        cout <<endl;
+    }
+}
