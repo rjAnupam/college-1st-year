e9f2abfc0fe39112bab44a80cb836861b47c5248
rjAnupam <janupam1704@gmail.com>
2023-01-21 12:31:27 +0530

    added 7/sorted

---
+++ 7/sorted.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+#define n 5
+
+
+void bubble_sort(int arr[]);
+
+int main(void)
+{
+	int numbers[n];
+
+	cout <<"Enter numbers to be sorted:\n";
+	for (int i = 0; i < n; i++)
+	{
+		cin >>numbers[i];
+	}
+
+	bubble_sort(numbers);
+
+	cout <<"\nOutput:\n";
+	for (int i = 0; i < n; i++)
+	{
+		cout <<numbers[i] <<endl;
+	}
+
+	return 0;
+}
+
+void bubble_sort(int arr[])
+{
+	for (int i = 0; i < n-1; i++)
+	{
+		for (int j = 0; j < n-1-i; j++)
+		{
+			if (arr[j] > arr[j+1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+}
